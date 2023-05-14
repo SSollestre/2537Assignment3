ee0f1c0fc42540cfa8156f9097d4075f7196ea03
Sean Sollestre <sean.sollestre@gmail.com>
2023-05-14 07:53:47 -0700

    Display response with a card for each pomeon

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const setup = async () => {
-    let response = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=810')
-    console.log(response.data.results)
+    let allPokemonResponse = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=810')
+    let allPokemon = allPokemonResponse.data.results
+    allPokemon.forEach(async (pokemonObject) => {
+        let pokemonResponse = await axios.get(pokemonObject.url)
+        let pokemon = pokemonResponse.data
+        let pokemonName = pokemon.name
+        let pokemonHeader = pokemonName.toUpperCase()
+        let pokemonImage = pokemon.sprites.front_default
+        $('#responseTest').append(`
+            <div class="pokeCard card" pokeName=${pokemonName}>
+            <h3>${pokemonHeader}</h3>
+            <img src="${pokemonImage}" alt="${pokemonName}">
+            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#pokeModal">More</button>
+        </div>
+    `);
+    })
+
 }
 
 
