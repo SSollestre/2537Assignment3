c1fa7271edbaab0d9189b329eaec02e18ff5017b
Sean Sollestre <sean.sollestre@gmail.com>
2023-05-14 08:08:16 -0700

    Limit pokemon to 10 per page

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+const PAGE_SIZE = 10
+let currentPage = 1
+
+
 const setup = async () => {
     let allPokemonResponse = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=810')
     let allPokemon = allPokemonResponse.data.results
-    allPokemon.forEach(async (pokemonObject) => {
+
+    let pagePokemon = allPokemon.slice((currentPage - 1) * PAGE_SIZE, currentPage * PAGE_SIZE)
+
+    pagePokemon.forEach(async (pokemonObject) => {
         let pokemonResponse = await axios.get(pokemonObject.url)
         let pokemon = pokemonResponse.data
         let pokemonName = pokemon.name
         let pokemonHeader = pokemonName.toUpperCase()
         let pokemonImage = pokemon.sprites.front_default
-        $('#responseTest').append(`
+        $('#pokeCardContainer').append(`
             <div class="pokeCard card" pokeName=${pokemonName}>
             <h3>${pokemonHeader}</h3>
             <img src="${pokemonImage}" alt="${pokemonName}">
